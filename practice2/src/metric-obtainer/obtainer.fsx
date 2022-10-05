bd0119f216b08685b4775543a4d89bfe73f8b295
Михаил Проказин <bearpro@outlook.com>
2022-10-06 00:28:43 +0300

    Отправка собранных данных в БД

--- practice2/src/metric-obtainer/obtainer.fsx
+++ practice2/src/metric-obtainer/obtainer.fsx
@@ -24,28 +24,46 @@ let weatherJson =
 
 open TargetModel
 
-let parseWeather json : Weather =
+let parseWeather json : Weather list =
   let sourceData = Json.deserialize<SourceJsonModel.Root> json
   let dataseries = sourceData.dataseries
-  let nearestForecast = 
-    dataseries 
-    |> List.minBy(fun x -> x.timepoint)
-  let time = DateTime(
-    year = (sourceData.init.Substring(0, 4) |> Int32.Parse),
-    month = (sourceData.init.Substring(4, 2) |> Int32.Parse),
-    day = (sourceData.init.Substring(6, 2) |> Int32.Parse))
-  { date = time; temperature = nearestForecast.temp2m }
+  
+  dataseries 
+  |> List.map (fun forecast -> 
+      let time = DateTime(
+        year = (sourceData.init.Substring(0, 4) |> Int32.Parse),
+        month = (sourceData.init.Substring(4, 2) |> Int32.Parse),
+        day = (sourceData.init.Substring(6, 2) |> Int32.Parse))
+      let hourOffset = sourceData.init.Substring(8, 2) |> Int32.Parse
+
+      let time = time.AddHours (float (forecast.timepoint + hourOffset))
+      { date = time; temperature = forecast.temp2m }
+    )
 
 
-let weather = Job.map parseWeather weatherJson
+let forecast = Job.map parseWeather weatherJson
 
 let main = 
+  let [|user; password |] = 
+    Environment.GetEnvironmentVariable("WEATHER_DB_CONN").Split(":")
+  let connectionString = $"Server=localhost;Port=5432;Database=weather;User Id={user};Password={password};"
+  
   job {
-    let! weather = weather
-    // TODO
-    // use connection = new NpgsqlConnection("")
-    // let! inserted = connection.ExecuteAsync("INSERT INTO metrics VALUES (@date, @weather)", weather)
-    ()
+    use connection = new NpgsqlConnection(connectionString)
+
+    let! forecast = forecast
+    let firstMetric = forecast |> List.minBy (fun x -> x.date)
+    
+    let! deleted = connection.ExecuteAsync(
+      "DELETE FROM metrics WHERE date >= @min_date", 
+      {| min_date = firstMetric.date |} )
+    printfn $"Deleted {deleted} rows"
+
+    for weather in forecast do
+      let! inserted = connection.ExecuteAsync(
+        "INSERT INTO metrics VALUES (@date, @temperature)", 
+        {| date = weather.date; temperature = weather.temperature |})
+      printfn $"Inserted {inserted} row, {weather}"
   }
 
 do run main
