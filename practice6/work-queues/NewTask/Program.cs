1c147fab49335415d679ba5562dbd4afa4574bc0
Михаил Проказин <bearpro@outlook.com>
2023-01-18 00:55:35 +0300

    Практика №6 часть 1

---
+++ practice6/work-queues/NewTask/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+static string GetMessage(string[] args) => args.Length switch
+{
+    > 0 => string.Join(" ", args),
+    _ => "Hello World!"
+};
+
+var factory = new ConnectionFactory() { 
+    HostName = "localhost",
+    UserName = "guest", 
+    Password = "guest",
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+
+channel.QueueDeclare(
+    queue: "task_queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null
+);
+
+channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+
+var message = GetMessage(args);
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+var properties = channel.CreateBasicProperties();
+properties.Persistent = true;
+
+channel.BasicPublish(
+    exchange: "",
+    routingKey: "task_queue",
+    basicProperties: null,
+    body: encodedMessage
+);
+
+Console.WriteLine($"Message '{message}' sent.");
---
+++ practice6/work-queues/Worker/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+static async Task<int> WaitSigterm()
+{
+    var cts = new CancellationTokenSource();
+    AppDomain.CurrentDomain.ProcessExit += (s, e) => {
+        Console.WriteLine("Recieved SIGTERM");
+        cts.Cancel();
+    };
+    await Task.Delay(-1, cts.Token);
+    Console.WriteLine("Stopped.");
+    return 0;
+}
+
+System.Console.WriteLine("Trying attach to 'dpt-rabbit'.");
+
+var factory = new ConnectionFactory() { 
+    HostName = "dpt-rabbit", 
+    UserName = "guest", 
+    Password = "guest",
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "task_queue",
+    durable: true,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"[x] Received '{message}'");
+
+    int dots = message.Split('.').Length - 1;
+    Thread.Sleep(dots * 1000);
+    
+    Console.WriteLine("[x] Done");
+
+    channel.BasicAck(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+channel.BasicConsume(
+    queue: "task_queue",
+    autoAck: false,
+    consumer: consumer);
+
+Console.WriteLine("[x] Started listening");
+
+// Infinite loop tool SIGTERM
+await WaitSigterm();
